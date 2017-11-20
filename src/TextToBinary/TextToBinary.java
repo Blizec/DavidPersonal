d3d639105d30af91f9a9f3d508f746a119b3473b
David Angeå <31401842+Blizec@users.noreply.github.com>
2017-11-20 11:02:50 +0100

    TextToBinary

--- src/TextToBinary/TextToBinary.java
+++ src/TextToBinary/TextToBinary.java
@@ -12,11 +12,9 @@ public class TextToBinary {
 		
 		  byte[] bytes = input.getBytes();
 		  StringBuilder binary = new StringBuilder();
-		  for (byte b : bytes)
-		  {
+		  for (byte b : bytes){
 		     int val = b;
-		     for (int i = 0; i < 8; i++)
-		     {
+		     for (int i = 0; i < 8; i++){
 		        binary.append((val & 128) == 0 ? 0 : 1);
 		        val <<= 1;
 		     }
